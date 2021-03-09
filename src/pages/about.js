30f19d3f4286360f12d190e84bcf8377d3ec80ce
svanheusden <velocio@gmail.com>
2021-03-09 21:48:50 +0100

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,7 +5,7 @@ export default function About() {
     <div style={{ color: `teal` }}>
       <h1>About Gatsby</h1>
       <p>Such wow. Very React.</p>
-    <Link to="/index/">Go home</Link>
+    <Link to="/">Go home</Link>
     </div>
   )
 }
