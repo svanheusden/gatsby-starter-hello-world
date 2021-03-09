9e95f059860f7a5b92f22960485d2f67f8cba6fe
svanheusden <velocio@gmail.com>
2021-03-09 21:23:41 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
 import React from "react"
+import Header from "../src/pages/header"
 
 export default function Home() {
   return <div>
-    <h1>H1 title</h1>
-    <h2>H2 title</h2>
+    <Header />
+    <h2>H2 subtitle</h2>
     <p>Hello world!</p>
   <img src="https://source.unsplash.com/random/400x200" alt="" />
   </div>
