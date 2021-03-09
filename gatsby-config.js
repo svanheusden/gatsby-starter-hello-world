c8844a47a69a1bb94ac2252d1c2c3ec457e7f9ad
svanheusden <velocio@gmail.com>
2021-03-09 22:10:29 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,12 +5,5 @@
  */
 
 module.exports = {
- plugins: [
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography`,
-      },
-    },
-  ],
+ plugins: [],
 }
