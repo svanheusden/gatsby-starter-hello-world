632c280bf2ac10073ca60c0c1f96160287a9c2a5
svanheusden <velocio@gmail.com>
2021-03-09 21:16:13 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,9 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <div>
+    <h1>H1 title</h1>
+    <h2>H2 title</h2>
+    <p>Hello world!</p>
+  </div>
 }
