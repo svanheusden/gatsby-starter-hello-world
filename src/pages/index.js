661c03cd34a73a68d5c1c2b09b943103be9be0cd
svanheusden <velocio@gmail.com>
2021-03-09 22:18:42 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Header from "../components/header"
 export default function Home() {
   return <div>
     <Header />
-    <h2>H2 subtitle</h2>
+    <h2>{data.site.siteMetadata.title}</h2>
     <p>Hello world!</p>
   <Link to="/about/">Contact</Link>
   <img src="https://source.unsplash.com/random/400x200" alt="" />
